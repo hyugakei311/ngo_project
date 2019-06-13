3dc464f7d030afa5dc70d01b8d68b2e08a70ae8a
pingvin65 <budai2002@gmail.com>
2019-06-13 15:59:57 -0400

    test
    Signed-off-by: pingvin65 <budai2002@gmail.com>

---
+++ ngo/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from .models import User, Event
+# Create your views here.
+
+
+class UserCreate(CreateView):
+    model = User
+    fields = "__all__"
